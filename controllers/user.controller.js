e2a635ab6fb2b550ad7838993748a94db4670d6a
Maria-colturi <mabecolturi@gmail.com>
2023-08-29 21:35:12 +0100

    chore: adding users controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,17 @@
+
+
+module.exports.create = (req, res, next) => {
+    res.render("users/create")
+}
+
+module.exports.doCreate = (req, res, next) => {
+    
+}
+
+module.exports.detail = (req, res, next) => {}
+
+module.exports.login = (req, res, next) => {}
+
+module.exports.doLogin = (req, res, next) => {}
+
+module.exports = User
