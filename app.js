c465d2c2d5557f32ba8901c6a9d69399ca777334
Maria-colturi <mabecolturi@gmail.com>
2023-08-30 19:15:44 +0100

    update: views config

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const hbs = require("hbs");
 const app = express();
 
 app.set("view engine", "hbs");
-app.set("views", `${__direname}/views`);
+app.set("views", `${__dirname}/views`);
 
 app.listen( 3000, () => {console.log("listen in port 3000")})
